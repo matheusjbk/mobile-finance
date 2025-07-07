ee8e3a77aa952ce95b709928b7b20d369b7639e5
matheusjbk <matheusjbk@gmail.com>
2025-07-07 16:59:04 -0300

    feat: adding user repository

---
+++ src/backend/MobileFinance.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace MobileFinance.Domain.Repositories.User;
+public interface IUserReadOnlyRepository
+{
+    Task<bool> ExistActiveUserWithEmail(string email);
+}
---
+++ src/backend/MobileFinance.Infra/DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileFinance.Domain.Entities;
+using MobileFinance.Domain.Repositories.User;
+
+namespace MobileFinance.Infra.DataAccess.Repositories;
+public class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository
+{
+    private readonly MobileFinanceDbContext _dbContext;
+
+    public UserRepository(MobileFinanceDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task<bool> ExistActiveUserWithEmail(string email) =>
+        await _dbContext.Users.AnyAsync(user => user.Active && user.Email == email);
+
+    public async Task Add(User user) => await _dbContext.Users.AddAsync(user);
+}
