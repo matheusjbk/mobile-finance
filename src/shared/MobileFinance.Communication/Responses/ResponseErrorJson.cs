90d15bb426043737183c4a33dd70a876329d3434
matheusjbk <matheusjbk@gmail.com>
2025-07-07 17:40:07 -0300

    feat: adding a custom exception and a exception filter

---
+++ src/shared/MobileFinance.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileFinance.Communication.Responses;
+public class ResponseErrorJson
+{
+    public IList<string> Errors { get; set; }
+    public bool TokenIsExpired { get; set; }
+
+    public ResponseErrorJson(IList<string> errors) => Errors = errors;
+
+    public ResponseErrorJson(string error) => Errors = [error];
+}
