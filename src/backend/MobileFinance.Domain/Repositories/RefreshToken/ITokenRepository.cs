4c4b2d933962a44ab8c3d8a9f30297426c18822c
matheusjbk <matheusjbk@gmail.com>
2025-07-08 17:01:54 -0300

    feat: adding token repository

---
+++ src/backend/MobileFinance.Domain/Repositories/RefreshToken/ITokenRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace MobileFinance.Domain.Repositories.RefreshToken;
+public interface ITokenRepository
+{
+    public Task<Entities.RefreshToken?> Get(string refreshToken);
+    public Task SaveNewRefreshToken(Entities.RefreshToken refreshToken);
+}
---
+++ src/backend/MobileFinance.Infra/DataAccess/Repositories/TokenRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileFinance.Domain.Entities;
+using MobileFinance.Domain.Repositories.RefreshToken;
+
+namespace MobileFinance.Infra.DataAccess.Repositories;
+public class TokenRepository : ITokenRepository
+{
+    private readonly MobileFinanceDbContext _dbContext;
+
+    public TokenRepository(MobileFinanceDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task<RefreshToken?> Get(string refreshToken) =>
+        await _dbContext.RefreshTokens
+        .AsNoTracking()
+        .Include(token => token.User)
+        .FirstOrDefaultAsync(token => token.Value.Equals(refreshToken));
+
+    public async Task SaveNewRefreshToken(RefreshToken refreshToken)
+    {
+        var tokens = _dbContext.RefreshTokens.Where(token => token.UserId.Equals(refreshToken.UserId));
+
+        _dbContext.RefreshTokens.RemoveRange(tokens);
+
+        await _dbContext.RefreshTokens.AddAsync(refreshToken);
+    }
+}
