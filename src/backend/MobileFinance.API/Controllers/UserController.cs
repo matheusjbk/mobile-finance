b946c41324c021d4400d1040915fec85460c1705
matheusjbk <matheusjbk@gmail.com>
2025-07-09 15:31:17 -0300

    feat: adding user controller and register user endpoint

---
+++ src/backend/MobileFinance.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobileFinance.Application.UseCases.User.Register;
+using MobileFinance.Communication.Requests;
+using MobileFinance.Communication.Responses;
+
+namespace MobileFinance.API.Controllers;
+public class UserController : MobileFinanceBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register(
+        IRegisterUserUseCase useCase,
+        RequestRegisterUserJson request)
+    {
+        var response = await useCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+}
