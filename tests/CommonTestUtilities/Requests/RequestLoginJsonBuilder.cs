7edb3a7d5c4fa526d266d63cfc4c533b59248588
matheusjbk <matheusjbk@gmail.com>
2025-07-10 16:39:23 -0300

    test: adding login unit and integration tests

---
+++ tests/CommonTestUtilities/Requests/RequestLoginJsonBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using MobileFinance.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestLoginJsonBuilder
+{
+    public static RequestLoginJson Build()
+    {
+        return new Faker<RequestLoginJson>()
+            .RuleFor(login => login.Email, f => f.Internet.Email())
+            .RuleFor(login => login.Password, f => f.Internet.Password());
+    }
+}
---
+++ tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,66 @@
+﻿using CommonTestUtilities.Requests;
+using MobileFinance.Communication.Requests;
+using MobileFinance.Exceptions;
+using Shouldly;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Test.Login.DoLogin;
+public class DoLoginTest : MobileFinanceClassFixture
+{
+    private const string ROUTE = "login";
+    private readonly string _userName;
+    private readonly string _userEmail;
+    private readonly string _userPassword;
+
+    public DoLoginTest(MobileFinanceWebApplicationFactory factory) : base(factory)
+    {
+        _userName = factory.GetUserName();
+        _userEmail = factory.GetUserEmail();
+        _userPassword = factory.GetUserPassword();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = new RequestLoginJson
+        {
+            Email = _userEmail,
+            Password = _userPassword
+        };
+
+        var response = await DoPost(ROUTE, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.ShouldSatisfyAllConditions(
+            () => responseData.RootElement.GetProperty("name").GetString().ShouldNotBeNullOrWhiteSpace(),
+            () => responseData.RootElement.GetProperty("name").GetString().ShouldBe(_userName),
+            () => responseData.RootElement.GetProperty("tokens").GetProperty("accessToken").GetString().ShouldNotBeNullOrEmpty(),
+            () => responseData.RootElement.GetProperty("tokens").GetProperty("refreshToken").GetString().ShouldNotBeNullOrEmpty());
+    }
+
+    [Fact]
+    public async Task Error_Invalid_User()
+    {
+        var request = RequestLoginJsonBuilder.Build();
+
+        var response = await DoPost(ROUTE, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ExceptionMessages.ResourceManager.GetString("INVALID_EMAIL_OR_PASSWORD");
+
+        errors.ShouldHaveSingleItem().GetString().ShouldBe(expectedMessage);
+    }
+}
