aafb6a93657472767608bb4d499e785fdf9274db
matheusjbk <matheusjbk@gmail.com>
2025-07-10 13:54:09 -0300

    test: adding entity builders

---
+++ tests/CommonTestUtilities/Entities/RefreshTokenBuilder.cs
@@ -0,0 +1,16 @@
+﻿using MobileFinance.Domain.Entities;
+
+namespace CommonTestUtilities.Entities;
+public class RefreshTokenBuilder
+{
+    public static RefreshToken Build(User user)
+    {
+        return new RefreshToken
+        {
+            Id = 1,
+            Value = Convert.ToBase64String(Guid.NewGuid().ToByteArray()),
+            UserId = user.Id,
+            User = user,
+        };
+    }
+}
