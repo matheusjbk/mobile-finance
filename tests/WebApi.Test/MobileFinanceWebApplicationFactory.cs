64592a0a37647c3c63e2a79091d1af5ba443dfd4
matheusjbk <matheusjbk@gmail.com>
2025-07-10 15:06:34 -0300

    test: adding integration tests to register an user

---
+++ tests/WebApi.Test/MobileFinanceWebApplicationFactory.cs
@@ -0,0 +1,61 @@
+﻿using CommonTestUtilities.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MobileFinance.Domain.Entities;
+using MobileFinance.Infra.DataAccess;
+
+namespace WebApi.Test;
+public class MobileFinanceWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private MobileFinance.Domain.Entities.User _user = default!;
+    private string _password = string.Empty;
+    private RefreshToken _refreshToken = default!;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test")
+            .ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MobileFinanceDbContext>));
+
+                if(descriptor is not null)
+                    services.Remove(descriptor);
+
+                var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                services.AddDbContext<MobileFinanceDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDatabase");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+
+                var dbContext = scope.ServiceProvider.GetRequiredService<MobileFinanceDbContext>();
+
+                StartDatabase(dbContext);
+            });
+    }
+
+    public string GetUserName() => _user.Name;
+    public string GetUserEmail() => _user.Email;
+    public Guid GetUserIdentifier() => _user.UserIdentifier;
+    public string GetUserPassword() => _password;
+
+    public RefreshToken GetRefreshToken() => _refreshToken;
+
+    private void StartDatabase(MobileFinanceDbContext dbContext)
+    {
+        (_user, _password) = UserBuilder.Build();
+        _refreshToken = RefreshTokenBuilder.Build(_user);
+
+        dbContext.Database.EnsureDeleted();
+
+        dbContext.Users.Add(_user);
+        dbContext.RefreshTokens.Add(_refreshToken);
+
+        dbContext.SaveChanges();
+    }
+}
