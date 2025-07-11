9c6e9e43e6de9bd6b0f696c9cb5a46a5639a8610
matheusjbk <matheusjbk@gmail.com>
2025-07-11 17:36:08 -0300

    feat: adding a jwt validator and new function on user's repository

--- src/backend/MobileFinance.Domain/Repositories/User/IUserReadOnlyRepository.cs
+++ src/backend/MobileFinance.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -2,5 +2,6 @@
 public interface IUserReadOnlyRepository
 {
     Task<bool> ExistActiveUserWithEmail(string email);
+    Task<bool> ExistActiveUserWithIdentifier(Guid userIdentifier);
     Task<Entities.User?> GetByEmail(string email);
 }
---
+++ src/backend/MobileFinance.Infra/Security/Tokens/AccessToken/Validator/JwtValidator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MobileFinance.Domain.Security.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MobileFinance.Infra.Security.Tokens.AccessToken.Validator;
+public class JwtValidator : JwtHandler, IAccessTokenValidator
+{
+    private readonly string _signingKey;
+
+    public JwtValidator(string signingKey) => _signingKey = signingKey;
+
+    public Guid ValidateAndGetUserIdentifier(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = SecurityKey(_signingKey),
+            ClockSkew = new TimeSpan(0)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out _);
+
+        var userIdentifier = principal.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+
+        return Guid.Parse(userIdentifier);
+    }
+}
