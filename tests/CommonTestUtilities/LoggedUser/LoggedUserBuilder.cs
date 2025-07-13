7248267dce97fdba348232132b61d9dfe2214d9c
matheusjbk <matheusjbk@gmail.com>
2025-07-12 23:19:49 -0300

    test: adding unit test to get an user profile

---
+++ tests/CommonTestUtilities/LoggedUser/LoggedUserBuilder.cs
@@ -0,0 +1,16 @@
+﻿using MobileFinance.Domain.Entities;
+using MobileFinance.Domain.Services;
+using Moq;
+
+namespace CommonTestUtilities.LoggedUser;
+public class LoggedUserBuilder
+{
+    public static ILoggedUser Build(User user)
+    {
+        var mock = new Mock<ILoggedUser>();
+
+        mock.Setup(x => x.GetUser()).ReturnsAsync(user);
+
+        return mock.Object;
+    }
+}
---
+++ tests/UseCases.Test/User/Profile/GetUserProfileUseCaseTest.cs
@@ -0,0 +1,30 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using MobileFinance.Application.UseCases.User.Profile;
+using Shouldly;
+
+namespace UseCases.Test.User.Profile;
+public class GetUserProfileUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        (var user, _) = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var response = await useCase.Execute();
+
+        response.ShouldNotBeNull();
+        response.Name.ShouldBe(user.Name);
+        response.Email.ShouldBe(user.Email);
+    }
+
+    private static GetUserProfileUseCase CreateUseCase(MobileFinance.Domain.Entities.User user)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var mapper = MapperBuilder.Build();
+
+        return new GetUserProfileUseCase(loggedUser, mapper);
+    }
+}
