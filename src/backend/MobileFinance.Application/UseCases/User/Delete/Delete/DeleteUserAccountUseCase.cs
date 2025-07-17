ed3e99f2690ae09ce454875fc6940bf612b74cbf
matheusjbk <matheusjbk@gmail.com>
2025-07-17 13:56:21 -0300

    feat: adding use cases to delete an user account

---
+++ src/backend/MobileFinance.Application/UseCases/User/Delete/Delete/DeleteUserAccountUseCase.cs
@@ -0,0 +1,22 @@
+﻿
+using MobileFinance.Domain.Repositories;
+using MobileFinance.Domain.Repositories.User;
+
+namespace MobileFinance.Application.UseCases.User.Delete.Delete;
+public class DeleteUserAccountUseCase : IDeleteUserAccountUseCase
+{
+    private readonly IUserDeleteOnlyRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteUserAccountUseCase(IUserDeleteOnlyRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Execute(Guid userIdentifier)
+    {
+        await _repository.DeleteAccount(userIdentifier);
+        await _unitOfWork.Commit();
+    }
+}
