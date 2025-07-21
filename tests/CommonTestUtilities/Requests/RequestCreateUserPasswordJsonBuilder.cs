c3eaa98856ccf58aec4dd6d96b3e9fed39f5dae0
matheusjbk <matheusjbk@gmail.com>
2025-07-21 00:46:02 -0300

    test: adding tests to create a password to an user

---
+++ tests/CommonTestUtilities/Requests/RequestCreateUserPasswordJsonBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Bogus;
+using MobileFinance.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestCreateUserPasswordJsonBuilder
+{
+    public static RequestCreateUserPasswordJson Build(int passwordLength = 10)
+    {
+        return new Faker<RequestCreateUserPasswordJson>()
+            .RuleFor(request => request.Password, f => f.Internet.Password(passwordLength));
+    }
+}
---
+++ tests/UseCases.Test/User/CreatePassword/CreateUserPasswordUseCaseTest.cs
@@ -0,0 +1,55 @@
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using MobileFinance.Application.UseCases.User.CreatePassword;
+using MobileFinance.Exceptions;
+using MobileFinance.Exceptions.ExceptionsBase;
+using Shouldly;
+
+namespace UseCases.Test.User.CreatePassword;
+public class CreateUserPasswordUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        (var user, _) = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+        var request = RequestCreateUserPasswordJsonBuilder.Build();
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        await act.ShouldNotThrowAsync();
+    }
+
+    [Fact]
+    public async Task Error_Empty_Password()
+    {
+        (var user, _) = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+        var request = RequestCreateUserPasswordJsonBuilder.Build();
+        request.Password = string.Empty;
+
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.GetErrorMessages().ShouldHaveSingleItem()
+            .ShouldBe(ExceptionMessages.EMPTY_PASSWORD);
+    }
+
+    private static CreateUserPasswordUseCase CreateUseCase(MobileFinance.Domain.Entities.User user)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var repository = new UserUpdateOnlyRepositoryBuilder().GetById(user).Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var passwordEncrypter = PasswordEncrypterBuilder.Build();
+
+        return new CreateUserPasswordUseCase(
+            loggedUser,
+            repository,
+            unitOfWork,
+            passwordEncrypter);
+    }
+}
