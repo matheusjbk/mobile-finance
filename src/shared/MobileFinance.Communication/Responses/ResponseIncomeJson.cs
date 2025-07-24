cb3e520752187939ca08e41a786f77f754599304
matheusjbk <matheusjbk@gmail.com>
2025-07-24 12:45:40 -0300

    feat: adding a new exception, a new exception message and a new response model

---
+++ src/shared/MobileFinance.Communication/Responses/ResponseIncomeJson.cs
@@ -0,0 +1,12 @@
+﻿using MobileFinance.Communication.Enums;
+
+namespace MobileFinance.Communication.Responses;
+public class ResponseIncomeJson
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title {  get; set; } = string.Empty;
+    public long Amount { get; set; }
+    public IncomeType IncomeType { get; set; }
+    public byte? DayOfMonth { get; set; }
+    public bool UseBusinessDay { get; set; }
+}
