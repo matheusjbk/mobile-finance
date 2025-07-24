bf1f80ea3273cf86153045bfbbe9ae4719b2fa78
matheusjbk <matheusjbk@gmail.com>
2025-07-24 16:27:39 -0300

    test: adding tests to get an income by your id

---
+++ tests/UseCases.Test/Income/GetById/GetIncomeByIdUseCaseTest.cs
@@ -0,0 +1,51 @@
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using MobileFinance.Application.UseCases.Income.GetById;
+using MobileFinance.Exceptions;
+using MobileFinance.Exceptions.ExceptionsBase;
+using Shouldly;
+
+namespace UseCases.Test.Income.GetById;
+public class GetIncomeByIdUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        (var user, _) = UserBuilder.Build();
+        var income = IncomeBuilder.Build(user);
+        var useCase = CreateUseCase(user, income);
+
+        var response = await useCase.Execute(income.Id);
+
+        response.ShouldNotBeNull();
+        response.Title.ShouldBe(income.Title);
+    }
+
+    [Fact]
+    public async Task Error_Income_NotFound()
+    {
+        (var user, _) = UserBuilder.Build();
+        var income = IncomeBuilder.Build(user);
+        var useCase = CreateUseCase(user);
+
+        Func<Task> act = async () => await useCase.Execute(income.Id);
+
+        var exception = await act.ShouldThrowAsync<NotFoundException>();
+
+        exception.GetErrorMessages().ShouldHaveSingleItem()
+            .ShouldBe(ExceptionMessages.INCOME_NOT_FOUND);
+    }
+
+    private static GetIncomeByIdUseCase CreateUseCase(MobileFinance.Domain.Entities.User user, MobileFinance.Domain.Entities.Income? income = null)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var readOnlyRepositoryBuilder = new IncomeReadOnlyRepositoryBuilder();
+
+        if(income is not null)
+            readOnlyRepositoryBuilder.GetById(user, income);
+
+        return new GetIncomeByIdUseCase(loggedUser, readOnlyRepositoryBuilder.Build());
+    }
+}
---
+++ tests/WebApi.Test/Income/GetById/GetIncomeByIdTest.cs
@@ -0,0 +1,63 @@
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Tokens;
+using MobileFinance.Exceptions;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Income.GetById;
+public class GetIncomeByIdTest : MobileFinanceClassFixture
+{
+    private const string ROUTE = "income";
+    private readonly Guid _userIdentifier;
+    private readonly string _incomeId;
+    private readonly string _incomeTitle;
+
+    public GetIncomeByIdTest(MobileFinanceWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+        _incomeId = factory.GetIncomeId();
+        _incomeTitle = factory.GetIncomeTitle();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var token = AccessTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+        var response = await DoGet(route: $"{ROUTE}/{_incomeId}", token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("id").GetString().ShouldBe(_incomeId);
+        responseData.RootElement.GetProperty("title").GetString().ShouldBe(_incomeTitle);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Income_NotFound(string culture)
+    {
+        var token = AccessTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+        var id = IdEncoderBuilder.Build().Encode(1000);
+
+        var response = await DoGet(route: $"{ROUTE}/{id}", token: token, culture: culture);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ExceptionMessages.ResourceManager.GetString("INCOME_NOT_FOUND", new CultureInfo(culture));
+
+        errors.ShouldHaveSingleItem().GetString().ShouldBe(expectedMessage);
+    }
+}
