4ec1c90b90936f09d76f1816435ce0c14d41ec2c
matheusjbk <matheusjbk@gmail.com>
2025-07-27 18:28:13 -0300

    feat: adding utilities to filter incomes by a day of month

---
+++ src/shared/MobileFinance.Communication/Responses/ResponseShortIncomeJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MobileFinance.Communication.Responses;
+public class ResponseShortIncomeJson
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public long Amount { get; set; }
+}
