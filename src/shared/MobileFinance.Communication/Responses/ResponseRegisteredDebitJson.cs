8cc09c6dc8c996d719134a41b77fc2df4597e80f
matheusjbk <matheusjbk@gmail.com>
2025-07-27 20:51:24 -0300

    feat: adding utilities to register a debit

---
+++ src/shared/MobileFinance.Communication/Responses/ResponseRegisteredDebitJson.cs
@@ -0,0 +1,6 @@
+﻿namespace MobileFinance.Communication.Responses;
+public class ResponseRegisteredDebitJson
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+}
