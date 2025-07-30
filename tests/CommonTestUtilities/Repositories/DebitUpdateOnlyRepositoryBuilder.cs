be763dd95a02786583e0bd03a3c02b46d82ce617
matheusjbk <matheusjbk@gmail.com>
2025-07-30 17:32:44 -0300

    feat: adding tests to update a debit by your id

---
+++ tests/CommonTestUtilities/Repositories/DebitUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,20 @@
+﻿using MobileFinance.Domain.Entities;
+using MobileFinance.Domain.Repositories.Debit;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
+public class DebitUpdateOnlyRepositoryBuilder
+{
+    private readonly Mock<IDebitUpdateOnlyRepository> _mock;
+
+    public DebitUpdateOnlyRepositoryBuilder() => _mock = new Mock<IDebitUpdateOnlyRepository>();
+
+    public DebitUpdateOnlyRepositoryBuilder GetById(User user, Debit debit)
+    {
+        _mock.Setup(repo => repo.GetById(user, debit.Id)).ReturnsAsync(debit);
+
+        return this;
+    }
+
+    public IDebitUpdateOnlyRepository Build() => _mock.Object;
+}
---
+++ tests/UseCases.Test/Debit/Update/UpdateDebitUseCaseTest.cs
@@ -0,0 +1,70 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using MobileFinance.Application.UseCases.Debit.Update;
+using MobileFinance.Exceptions;
+using MobileFinance.Exceptions.ExceptionsBase;
+using Shouldly;
+
+namespace UseCases.Test.Debit.Update;
+public class UpdateDebitUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        (var user, _) = UserBuilder.Build();
+        var debit = DebitBuilder.Build(user);
+        var request = RequestDebitJsonBuilder.Build();
+        var useCase = CreateUseCase(user, debit);
+
+        Func<Task> act = async () => await useCase.Execute(request, debit.Id);
+
+        await act.ShouldNotThrowAsync();
+    }
+
+    [Fact]
+    public async Task Error_Debit_NotFound()
+    {
+        (var user, _) = UserBuilder.Build();
+        var debit = DebitBuilder.Build(user);
+        var request = RequestDebitJsonBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        Func<Task> act = async () => await useCase.Execute(request, debit.Id);
+
+        var exception = await act.ShouldThrowAsync<NotFoundException>();
+
+        exception.GetErrorMessages().ShouldHaveSingleItem()
+            .ShouldBe(ExceptionMessages.DEBIT_NOT_FOUND);
+    }
+
+    [Fact]
+    public async Task Error_Empty_Title()
+    {
+        (var user, _) = UserBuilder.Build();
+        var debit = DebitBuilder.Build(user);
+        var request = RequestDebitJsonBuilder.Build();
+        request.Title = string.Empty;
+        var useCase = CreateUseCase(user, debit);
+
+        Func<Task> act = async () => await useCase.Execute(request, debit.Id);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.GetErrorMessages().ShouldHaveSingleItem()
+            .ShouldBe(ExceptionMessages.EMPTY_DEBIT_TITLE);
+    }
+
+    private static UpdateDebitUseCase CreateUseCase(MobileFinance.Domain.Entities.User user, MobileFinance.Domain.Entities.Debit? debit = null)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var repositoryBuilder = new DebitUpdateOnlyRepositoryBuilder();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        if(debit is not null)
+            repositoryBuilder.GetById(user, debit);
+
+        return new UpdateDebitUseCase(loggedUser, repositoryBuilder.Build(), unitOfWork);
+    }
+}
