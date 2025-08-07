117951803b0a11dc4dce13e20f50e587311b1509
matheusjbk <matheusjbk@gmail.com>
2025-08-07 17:07:25 -0300

    feat: adding function to get a debit by your date and adding models

--- src/backend/MobileFinance.Domain/Repositories/Debit/IDebitReadOnlyRepository.cs
+++ src/backend/MobileFinance.Domain/Repositories/Debit/IDebitReadOnlyRepository.cs
@@ -1,5 +1,6 @@
 ﻿namespace MobileFinance.Domain.Repositories.Debit;
 public interface IDebitReadOnlyRepository
 {
-    public Task<Entities.Debit?> GetById(Entities.User user, long debitId); 
+    public Task<Entities.Debit?> GetById(Entities.User user, long debitId);
+    public Task<IList<Entities.Debit>> GetByDayOfMonth(Entities.User user, DateTime date);
 }
