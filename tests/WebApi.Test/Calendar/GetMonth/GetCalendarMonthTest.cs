55b9f019bb0398da80337ac47d3f920e00c6f630
matheusjbk <matheusjbk@gmail.com>
2025-08-29 15:39:27 -0300

    test: integration tests for get a calendar day or month

---
+++ tests/WebApi.Test/Calendar/GetMonth/GetCalendarMonthTest.cs
@@ -0,0 +1,37 @@
+﻿using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Text.Json;
+
+namespace WebApi.Test.Calendar.GetMonth;
+public class GetCalendarMonthTest : MobileFinanceClassFixture
+{
+    private const string ROUTE = "calendar/month";
+    private readonly Guid _userIdentifier;
+    private readonly DateTime _incomeReceivedOn;
+
+    public GetCalendarMonthTest(MobileFinanceWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+        _incomeReceivedOn = factory.GetIncomeReveicedDate();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var token = AccessTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+        var year = _incomeReceivedOn.Year;
+        var month = _incomeReceivedOn.Month;
+
+        var response = await DoGet(route: $"{ROUTE}?year={year}&month={month}", token: token);
+
+        response.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        responseData.RootElement.GetProperty("year").GetInt32().ShouldBe(year);
+        responseData.RootElement.GetProperty("month").GetInt32().ShouldBe(month);
+        responseData.RootElement.GetProperty("days").EnumerateArray().ShouldNotBeEmpty();
+    }
+}
